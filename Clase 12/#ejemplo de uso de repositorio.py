c91ce65cf8e978d09e419b1df95be307fb8d1e08
Reinkiri <134543117+Reinkiri@users.noreply.github.com>
2023-05-24 14:34:14 -0400

    archivo datos.py sin validar

---
+++ Clase 12/#ejemplo de uso de repositorio.py
@@ -0,0 +1,11 @@
+#ejemplo de uso de repositorio
+
+
+print("datos personales")
+print("<><><><><><><><><><><><><><><><>")
+Vnom=input("ingrese su nombre:")
+VEdad= input("ingrese su edad:")
+print("<><><><><><><><><><><><><><><><>")
+print(f"Su nombre es:{Vnom}")
+print(f"Su edad es:{VEdad} ")
+print("fin el programa............")
